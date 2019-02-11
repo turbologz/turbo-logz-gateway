8ad76b1d48b441ba0c3ecbfce9df73f4b804eff4
el-davo <david.ahrn26@gmail.com>
2019-02-11 21:31:45 +0000

    Adding tests for user interface and graphql

---
+++ test/subscriptions.spec.ts
@@ -0,0 +1,32 @@
+import {expect} from 'chai';
+import * as supertest from 'supertest';
+import * as express from 'express';
+import '../server';
+
+describe('Graphql Route', () => {
+
+    let request: any;
+
+    beforeEach(async () => {
+        request = supertest("http://localhost:8080");
+
+        const app = express();
+        const port = 3002;
+
+        app.get('/graphql', (req, res) => res.send({type: 'graphql'}));
+
+        await new Promise((resolve) => {
+            app.listen(port, () => resolve())
+        });
+    });
+
+    it('should communicate with graphql at /graphql route', (done) => {
+        request
+            .get("/graphql")
+            .expect(200)
+            .then((response: any) => {
+                expect(response.body).to.eql({type: 'graphql'});
+                done();
+            })
+    });
+});
